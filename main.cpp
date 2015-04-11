82f019b8a7a63d6915466394918eee049f342119
Kevin Michael Allen <kma1660@gmail.com>
2015-04-11 16:45:52 -0400

    rewrote test executable to test recording, writing, resettting, reading all for 3000 loops

--- main.cpp
+++ main.cpp
@@ -5,19 +5,36 @@ using namespace std;
 int main() {
 	Recorder* rec = Recorder::GetInstance();
 
+	//One of each device
 	TestDevice* dev = new TestDevice("dev");
-
 	Relay* rel = new Relay();
+	SpeedController* ctrl = new SpeedController();
+	Servo* serv = new Servo();
+	DoubleSolenoid* ds = new DoubleSolenoid();
+	Solenoid* sol = new Solenoid();
 
-	rec->AddDevice("relay",rel);
+	//Add all devices to recorder
+	rec->AddDevice("Relay",rel);
+	rec->AddDevice("Speed Controller",ctrl);
+	rec->AddDevice("Servo",serv);
+	rec->AddDevice("Double Solenoid",ds);
+	rec->AddDevice("Solenoid",sol);
 	rec->AddDevice(dev);
 
+	//Creates macro
 	Macro* mac = rec -> macro();
 
-	mac->Record();
-	mac->Record();
-	mac->WriteFile("saves/new.csv");
+
+	for (int i = 0; i<3000; i++) {
+		mac->Record();
+	}
+
+	mac->WriteFile("saves/auto.csv");
+	mac->Reset();
+
+	mac->ReadFile("saves/auto.csv");
+	while (!mac->IsFinished()) mac->PlayBack();
 
 	std::cout << "plz work" << std::endl;
-	return 0;
+
 }
